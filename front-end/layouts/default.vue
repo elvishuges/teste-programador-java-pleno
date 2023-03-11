f631865bd324f2ac91510aa895d97d1ccfd48038
Elvis Huges <elvis.huges@blockhub.builders>
2023-03-11 18:18:58 -0300

    add component nav bar

---
+++ front-end/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="default-page bg-gray-100">
+    <Nav />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Nav from "../components/nav.vue";
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
+
+<style></style>
--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-  name: "IndexPagedd",
+  name: "IndexPage",
 };
 </script>
